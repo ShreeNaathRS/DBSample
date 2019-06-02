689bfed2f313c96e1857182ff96d15fcbf4b3d94
ShreeNaathRS <rsshreenaath.com>
2019-06-02 18:51:54 +0530

    Angular

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const createController = require('../app/components/create/create.model');
-
-router.post('/create', createController.create);
-
-module.exports = router;
